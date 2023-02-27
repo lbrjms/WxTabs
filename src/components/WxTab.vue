9e9e4a8af4ebd3d6024e5c5934fe0c8e7eb14ed0
lwj <liuwenji@ms-battery.cn>
2023-02-27 18:25:58 +0800

    init

---
+++ src/components/WxTab.vue
@@ -0,0 +1,39 @@
+
+<script>
+import TabNav from './tab-nav.vue'
+export default {
+  name:'WxTabs',
+  components:{
+    TabNav
+  },
+  render(h) {
+    console.log('h :', h);
+    let {currentName} = this
+    const navData = {
+      ref: 'nav',
+      props: {
+        currentName
+      }
+    }
+    const header= (
+      <div>
+        <tab-nav {...navData}></tab-nav>
+      </div>
+    )
+    const panels= (
+      <div>
+        {this.$slots.default}
+      </div>
+    )
+    return (
+      <div>
+        {[header,panels]}
+      </div>
+    )
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tab-nav.vue
@@ -0,0 +1,25 @@
+
+<script>
+import TabBar from './tab-bar.vue';
+export default {
+    name:'TabNav',
+    components:{
+        TabBar
+    },
+    render(h) {
+        console.log('h :', h);
+        const tabs = {}
+        const panes = {}
+        return (
+            <div>
+              {<tab-bar tabs={panes}></tab-bar>}
+              {tabs}
+            </div>
+        )
+    }
+}
+</script>
+
+<style>
+
+</style>
